e8d23ee296e90cd371f91a00e3b5a3e64d4162c6
dharmite <dharmite.prabhudas@aeler.com>
2024-12-23 17:36:05 +0000

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-movies-tv-shows-rating/'
+  base: 'react-movies-tv-shows-rating'
 })
